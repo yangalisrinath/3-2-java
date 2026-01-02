2f0e8917ccba5f23eb4a3f82fbdba5fa78e316e9
yangalisrinath <srinathnethayangali@gmail.com>
2026-01-02 09:30:39 +0530

    Add files via upload

---
+++ Multiple perameters.java
@@ -0,0 +1,9 @@
+class MultipleParameters {
+    public static void main(String[] args) {
+        add(5, 10);
+    }
+
+    static void add(int a, int b) {
+        System.out.println(a + b);
+    }
+}
