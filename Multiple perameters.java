cf9d19d05193e7f563f97364722f067270a0d131
yangalisrinath <srinathnethayangali@gmail.com>
2026-01-03 09:39:04 +0530

    Delete Multiple perameters.java

--- Multiple perameters.java
+++
@@ -1,9 +0,0 @@
-class MultipleParameters {
-    public static void main(String[] args) {
-        add(5, 10);
-    }
-
-    static void add(int a, int b) {
-        System.out.println(a + b);
-    }
-}
